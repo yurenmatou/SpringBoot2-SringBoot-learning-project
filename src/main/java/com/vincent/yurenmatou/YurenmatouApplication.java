7ea945de53378919ee50bb638f8d9167cafe82f3
huliangyuan <386427280@qq.com>
2019-10-29 13:47:16 +0800

    新建空项目

---
+++ src/main/java/com/vincent/yurenmatou/YurenmatouApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.yurenmatou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YurenmatouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YurenmatouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vincent/yurenmatou/YurenmatouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.yurenmatou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YurenmatouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
