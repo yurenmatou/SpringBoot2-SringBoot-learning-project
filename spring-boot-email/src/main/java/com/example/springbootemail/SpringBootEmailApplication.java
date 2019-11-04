486a7e5fd80785bca9a7b82aef6e520e3a5161c7
huliangyuan <386427280@qq.com>
2019-11-04 10:21:54 +0800

    邮件

---
+++ spring-boot-email/src/main/java/com/example/springbootemail/SpringBootEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEmailApplication.class, args);
+    }
+
+}
---
+++ spring-boot-email/src/test/java/com/example/springbootemail/SpringBootEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
