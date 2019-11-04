264b48f7afcbc2865c280f0bddd316cff8649fcc
huliangyuan <386427280@qq.com>
2019-11-04 11:47:16 +0800

    web案例

---
+++ spring-boot-web/src/test/java/com/vincent/springbootweb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
