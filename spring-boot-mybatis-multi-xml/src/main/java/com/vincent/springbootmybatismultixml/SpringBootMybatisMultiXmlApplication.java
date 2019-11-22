0db7c4b28ae697cdd5cef712d2d484c172129a41
huliangyuan <386427280@qq.com>
2019-11-22 11:55:57 +0800

    提交mybatis 多数据源

---
+++ spring-boot-mybatis-multi-xml/src/main/java/com/vincent/springbootmybatismultixml/SpringBootMybatisMultiXmlApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.springbootmybatismultixml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisMultiXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisMultiXmlApplication.class, args);
+    }
+
+}
---
+++ spring-boot-mybatis-multi-xml/src/test/java/com/vincent/springbootmybatismultixml/SpringBootMybatisMultiXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.springbootmybatismultixml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisMultiXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
